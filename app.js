c325850a0dc0819f157f8744f233d3ae6e954575
Charlie Migwi <charliemigwi@gmail.com>
2019-12-13 15:02:11 +0300

    Set message for losing

--- app.js
+++ app.js
@@ -28,12 +28,65 @@ guessBtn.addEventListener("click", function() {
   let guess = parseInt(guessInput.value);
 
   //validate input
-  if (guess === isNaN || guess < min || guess > max) {
-    setMessage(`Plase Enter a Number between ${min} and ${max}`);
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Plase Enter a Number between ${min} and ${max}`, "red");
+  }
+
+  //Check for winning number
+  if (guess === winningNum) {
+    // //Won- Game over
+    // // Disable input
+    // guessInput.disabled = true;
+    // //Change border to green
+    // guessInput.style.borderColor = "green";
+    // //Let user know they won
+    // setMessage(`Good Guess,  ${winningNum} Is Correct, YOU WON!`, "green");
+  } else {
+    //Wrong Number
+    guessesLeft = guessesLeft - 1;
+    if (guessesLeft === 0) {
+      // Gameover- lost
+      // Disable input
+      guessInput.disabled = true;
+
+      //Change border to red
+      guessInput.style.borderColor = "red";
+
+      //Let user know they won
+      setMessage(
+        `Game over you are out of guesses, the correct number was ${winningNum}`,
+        "red"
+      );
+    } else {
+      //Change border to red
+      guessInput.style.borderColor = "red";
+
+      //Clear Input
+      guessInput.value = "";
+
+      // Notify users of guesses left
+      setMessage(
+        `${guess} is not correct, You have ${guessesLeft} guesse(s) left`,
+        "red"
+      );
+    }
   }
 });
 
+//Game Over
+function gameOver(won, msg) {
+  // Disable input
+  guessInput.disabled = true;
+
+  //Change border to green
+  guessInput.style.borderColor = "green";
+
+  //Let user know they won
+  setMessage(`Good Guess,  ${winningNum} Is Correct, YOU WON!`, "green");
+}
+
 //setMessage
-function setMessage(msg) {
+function setMessage(msg, color) {
+  message.style.color = color;
   message.textContent = msg;
 }
