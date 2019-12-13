8e9ba36289a57f5946b54ded9297a589e203f763
Charlie Migwi <charliemigwi@gmail.com>
2019-12-13 11:19:54 +0300

    add game rules

---
+++ app.js
@@ -0,0 +1,6 @@
+// Game Function and Rules
+// -Player must guess a number between a min and a max
+// -Player gets a certain amount of guesses
+// -Game notifies the player of the guesses remaining
+// -Game notifies the player of the correct answer if they loose
+// -Game lets the player choose to play again
