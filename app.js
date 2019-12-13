c4dcd7afccc8cd2ea6a29d45bd115908611f6dc4
Charlie Migwi <charliemigwi@gmail.com>
2019-12-13 12:55:11 +0300

    initiate UI Variables

--- app.js
+++ app.js
@@ -4,3 +4,17 @@
 // -Game notifies the player of the guesses remaining
 // -Game notifies the player of the correct answer if they loose
 // -Game lets the player choose to play again
+
+//Game Values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+//UI Elements
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
